dd9afe35532b5c8336703d727a9e20bfd7ec261e
Andrew Popko <popkoandr@gmail.com>
2016-10-10 09:10:37 +0300

    Implemented EmployeeService and EmployeeServiceHost

---
+++ Server/EmployeeService/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EmployeeService
+{
+    [DataContract(Namespace = "http://pragimtech.com/Employee")]
+    public class Employee
+    {
+        private int _id;
+        private string _name;
+        private string _gender;
+        private DateTime _dateOfBirth;
+
+        [DataMember(Order = 1)]
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        [DataMember(Order = 2)]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        [DataMember(Order = 3)]
+        public string Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        [DataMember(Order = 4)]
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+    }
+}
